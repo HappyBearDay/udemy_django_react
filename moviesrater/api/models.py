28262a22ab56ab9a213bbb1c2227cb96f299a556
anishamroun <anishamroun2@gmail.com>
2020-03-14 01:03:21 +0100

    [MoviesRater] 37.Ratings

--- moviesrater/api/models.py
+++ moviesrater/api/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
+import statistics
 # Create your models here.
 
 class Movie( models.Model):
         title = models.CharField(max_length=32)
         description = models.CharField(max_length=32)
+
+        def stats_of_ratings(self):
+                ratings = Rating.objects.filter(movie=self)
+                return ratings.aggregate(models.Avg('stars'), models.Count('stars'))
+        
+        def __str__(self):
+                return self.title
 
 class Rating(models.Model):
         movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='ratings')
