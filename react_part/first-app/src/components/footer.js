6dd45a191d6b9bba67e3c74da1ee7210034e048b
anishamroun <anishamroun2@gmail.com>
2020-03-14 17:35:44 +0100

    [React-Introduction] 44.Components

---
+++ react_part/first-app/src/components/footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+
+class Footer extends Component{
+    
+    
+    render () {
+        return (<h2> This is our FOOTER</h2>)
+    }
+}
+
+export default Footer;
---
+++ react_part/first-app/src/components/header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Header(){
+    return <h2> This is our HEADER</h2>
+}
+
+export {Header};
