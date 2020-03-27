4c41a3d4fdadb51dca9f13d3d8dc1ba76856c259
anishamroun <anishamroun2@gmail.com>
2020-03-27 23:27:35 +0100

    [React-Introduction] 54.Hooks - effect

--- react_part/first-app/src/components/numbers.js
+++ react_part/first-app/src/components/numbers.js
@@ -1,20 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 
 const Number = () => {
     const [numbers, setNumbers] = useState(['one', 'two', 'three'])
+    const [letters, setLetters] = useState(['a', 'b', 'c'])
 
     const addNumber = () => {
         setNumbers([...numbers, 'four'])
     }
+    
+    const addLetter = () => {
+        setLetters([...letters, 'd'])
+    }
+
+    useEffect( () =>{
+        console.log('our use effect triggers number')
+    }, [numbers])
+
+    useEffect( () =>{
+        console.log('our use effect triggers letter')
+    }, [letters])
+
     return (
         <div>
             <h1>Numbers</h1>
             { numbers.map( (num, index) => {
                 return (<h4 key={index}>{num}</h4>)
             })}
+
+            { letters.map( (num, index) => {
+                return (<h4 key={index}>{num}</h4>)
+            })}
+
             <button onClick={addNumber}>add a number</button>
+            <button onClick={addLetter}>add a letter</button>
         </div>
     )
 }
